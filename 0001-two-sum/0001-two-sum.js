8ee7d730a79d735b438681db9266ce5dab20a2dd
codingmoney <64386992+codingmoney@users.noreply.github.com>
2022-12-14 07:56:57 -0800

    Time: 74 ms (93.73%), Space: 42.8 MB (39.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -2,27 +2,29 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
-
+i 0,1,2
  [3,2,4], target = 6
 
  Hashmap { value : index }
+ {
+ 3:0,
+ 2:1,
  
+ }
  numTofind = target - currentNum
 
  */
 var twoSum = function(nums, target) {
+    const hashMap = {}
     
-  const hashMap = {}
-  
-  for(let i=0; i<nums.length; i++){
-      const num = nums[i]
-      const numToFind = target - num
-      
-      if(hashMap[numToFind]>=0){
-          return [hashMap[numToFind], i]
-      }else{
-          hashMap[num] = i
-      }
-  }
-    
+    for(let i=0; i<nums.length; i++){
+        const currentNum = nums[i]
+        const numToFind = target - currentNum
+        
+        if(hashMap[numToFind] >= 0 ){
+            return [hashMap[numToFind], i]
+        }else{
+            hashMap[currentNum] = i
+        }
+    }
 };
